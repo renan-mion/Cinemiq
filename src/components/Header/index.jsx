3a1210e578101f072639e3ad93e848c842743ccc
renan-mion <rnunesmion@gmail.com>
2024-12-15 19:18:31 -0300

    style: estilizado header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
+import './style.css';
+import Logo from '../../assets/images/logo.png'
 
 function Header() {
-    return(
+    return (
         <header>
-            <Link to='/'>Home</Link>
-            <Link to='/'>Meus Filmes</Link>
-            <Link to='/sobre'>Sobre</Link>
+            <img src={Logo} alt="logo"/>
+
+            <div className='links'>
+                <Link to='/'>Home</Link>
+                <Link to='/'>Meus Filmes</Link>
+                <Link to='/sobre'>Sobre</Link>
+            </div>
+
         </header>
     )
 }

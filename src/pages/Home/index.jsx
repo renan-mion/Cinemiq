008aad9c71e67fbf9b27183a6e15513511be5912
renan-mion <rnunesmion@gmail.com>
2024-12-15 20:02:39 -0300

    feat: iniciado uso da api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,41 @@
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import base_url from '../../services/base_url';
+import api_key from '../../services/api_key';
 
 function Home() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        async function loadFilmes() {
+            const response = await base_url.get('movie/now_playing', {
+                params: {
+                    api_key: api_key,
+                    language: 'pt-BR',
+                    page: 1
+                }
+            })
+
+            const resultados = response.data.results;
+
+            console.log(resultados);
+            setFilmes(resultados);
+        }
+
+        loadFilmes();
+
+    }, [])
+
     return(
-        <div>
-            <h1>Bem vindo á home</h1>
+        <div className="filmes">
+            {filmes.map((filme) => {
+                return (
+                    <div key={filme} className="filme-card">
+                        <h1 className="titulo-filme">{filme.title}</h1>
+                    </div>
+                    
+                )
+            })}
         </div>
     )
 }
---
+++ src/services/api_key.jsx
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const api_key = 'f57e5006231d62f99c09df686bf981dc';
+
+export default api_key;

6b5dab7c22a6efc41a029c77590e50c008ade929
renan-mion <rnunesmion@gmail.com>
2024-12-15 18:30:53 -0300

    commit inicial

---
+++ src/pages/Filme/index.jsx
@@ -0,0 +1,11 @@
+
+
+function Filme() {
+    return(
+       <div>
+            <h1>Bem vindo á pagina de detalhes do filme</h1>
+       </div> 
+    )
+}
+
+export default Filme;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+
+
+function Home() {
+    return(
+        <div>
+            <h1>Bem vindo á home</h1>
+        </div>
+    )
+}
+
+export default Home;
